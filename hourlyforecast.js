ede4f531e99a5a277fa5621b3d0f508edf2abbda
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-06-27 11:27:49 +0300

    Add files via upload

--- hourlyforecast.js
+++ hourlyforecast.js
@@ -45,16 +45,16 @@ Module.register("hourlyforecast",{
 			"01d": "wi-day-sunny",
 			"02d": "wi-day-cloudy",
 			"03d": "wi-cloudy",
-			"04d": "wi-day-cloudy-windy",
-			"09d": "wi-day-showers",
-			"10d": "wi-day-rain",
-			"11d": "wi-day-thunderstorm",
-			"13d": "wi-day-snow",
-			"50d": "wi-day-fog",
+			"04d": "wi-cloudy-windy",
+			"09d": "wi-showers",
+			"10d": "wi-rain",
+			"11d": "wi-thunderstorm",
+			"13d": "wi-snow",
+			"50d": "wi-fog",
 			"01n": "wi-night-clear",
-			"02n": "wi-night-alt-cloudy",
+			"02n": "wi-night-cloudy",
 			"03n": "wi-night-cloudy",
-			"04n": "wi-night-cloudy-windy",
+			"04n": "wi-night-cloudy",
 			"09n": "wi-night-showers",
 			"10n": "wi-night-rain",
 			"11n": "wi-night-thunderstorm",
@@ -144,7 +144,7 @@ Module.register("hourlyforecast",{
 
 			var degreeLabel = "";
 			if (this.config.units === "metric" || this.config.units === "imperial") {
-				degreeLabel += "&deg;";
+				degreeLabel += "°";
 			}
 			if(this.config.scale) {
 				switch(this.config.units) {
@@ -190,7 +190,7 @@ Module.register("hourlyforecast",{
 			}
 
 			var winter = moment().format("MM");
-    	    if ((winter >= "01" && winter <= "03") || (winter >= "11" && winter <= "12")) {
+			if ((winter >= "01" && winter <= "03") || (winter >= "11" && winter <= "12")) {
 				if (this.config.showSnowAmount) {
 					var snowCell = document.createElement("td");
 					if (isNaN(forecast.snow)) {
